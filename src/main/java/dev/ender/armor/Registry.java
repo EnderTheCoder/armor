2be3061829755fec561f62e9f3992750ec2f1962
ender <ggameinvader@gmail.com>
2023-04-13 19:24:22 +0800

    initial commit

--- src/main/java/dev/ender/armor/Registry.java
+++ src/main/java/dev/ender/armor/Registry.java
@@ -1,2 +1,18 @@
-package dev.ender.armor;public class Registry {
+package dev.ender.armor;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+
+public class Registry {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Item.class, Armor.MOD_ID);
+
+    public static final RegistryObject<Item> CN_ARMOR = ITEMS.register("cn_armor", () ->
+            new ArmorItem(ModArmorMaterial.CLOTHES, EquipmentSlotType.CHEST, new Item.Properties())
+    );
+
+
 }
